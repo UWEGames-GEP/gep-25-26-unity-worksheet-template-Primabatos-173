5d60b3b2f0a228dfc9f1585ae7b0d3cd4a298ffe
Yat Tim <Yat4.Chan@live.uwe.ac.uk>
2025-10-13 11:29:57 +0100

    pause function added

---
+++ Assets/Manager/Game manager.cs
@@ -0,0 +1,73 @@
+using System;
+using UnityEditor;
+using UnityEditor.MemoryProfiler;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class Gamemanager : MonoBehaviour
+{
+    public enum GameState {PAUSE,GAMEPLAY};
+    public GameState state;
+
+    bool haschangedstate = true;
+
+
+
+
+
+
+
+
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == GameState.GAMEPLAY)
+        {
+            if(Input.GetKeyDown(KeyCode.P))
+            {
+                state = GameState.PAUSE;
+                haschangedstate = true;
+            }
+        }
+        else if(state == GameState.PAUSE)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                state = GameState.GAMEPLAY;
+                haschangedstate = true;
+            }
+     
+        }
+        print(Time.timeScale);
+        
+        
+
+    }
+    private void LateUpdate()
+    {
+       
+
+
+        if (haschangedstate)
+        {
+            haschangedstate = false;
+
+            if (state == GameState.GAMEPLAY)
+            {
+                Time.timeScale = 1.0f;
+            }
+            else if (state == GameState.PAUSE)
+            {
+                Time.timeScale = 0.0f;
+            }
+        }
+    }
+}
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
